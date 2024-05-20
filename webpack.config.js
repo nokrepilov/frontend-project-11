fee1aa21b132d37c3c3989e3a9e37711168a2627
Nikita Okrepilov <nokrepilov@yandex.ru>
2024-05-20 12:24:50 +0300

    first step. setting up the environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-import HtmlWebpackPlugin from "html-webpack-plugin";
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-export default {
+module.exports = {
   mode: process.env.NODE_ENV || "development",
   module: {
     rules: [
